8c1c4e02b8e1fd11390045152ee0f476c2e6348a
Eresvet <remi32450@gmail.com>
2024-03-09 21:32:05 +0100

    faire : la quantite doit etre positive

--- src/histoire/ScenarioCasDegrade.java
+++ src/histoire/ScenarioCasDegrade.java
@@ -9,8 +9,16 @@ import villagegaulois.Village;
 public class ScenarioCasDegrade {
 	
 	public static void main(String[] args) {
-		Etal etal = new Etal();
-		etal.libererEtal();
+		Village village = new Village("le village des irréductibles", 10, 5);
+		Chef abraracourcix = new Chef("Abraracourcix", 10, village);
+		Gaulois bonemine = new Gaulois("Bonemine", 7);
+		village.setChef(abraracourcix);
+		village.ajouterHabitant(bonemine);
+		
+		System.out.println(village.installerVendeur(bonemine, "fleurs", 20));
+		Etal etalFleur = village.rechercherEtal(bonemine);
+
+		System.out.println(etalFleur.acheterProduit(10, abraracourcix));
 		System.out.println("Fin du test");
 		}
 	
